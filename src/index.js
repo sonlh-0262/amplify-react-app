f11615e340d0c81fde1e27d65bb9d8f5bcf05952
hungsonsn <hungson91cb@gmail.com>
2021-02-26 23:57:29 +0700

    add aws auth

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import config from './aws-export';
+
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
